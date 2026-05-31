419b02020b80872e29c7937e69bb2bb18c94eb08
2425405043 <2425405043@live.gctu.edu.gh>
2026-05-31 06:36:42 +0000

    Isaac Appiah's assigment

---
+++ initials.py
@@ -0,0 +1,33 @@
+# File: initials.py
+# Description: Prints stylized large initials with specific grid constraints.
+# Assignment Number: 1
+# 
+# Name: Isaac Appiah
+# STUDENT ID:  2425405043
+# Email: 2425405043@live.gctu.edu.gh
+# Grader: Mr August Buckman
+# 
+# On my honor, Isaac Appiah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    print()
+    print("...IAA")
+    print()
+    
+    print("...IIIIIIIIIIII........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("...IIIIIIIIIIII........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print(".......II..............AA........AA........AA........AA.....")
+    print(".......II..............AA........AA........AA........AA.....")
+    print(".......II..............AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print(".......II..............AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print(".......II..............AA........AA........AA........AA.....")
+    print(".......II..............AA........AA........AA........AA.....")
+    print("...IIIIIIIIIIII...**...AA........AA...**...AA........AA....**")
+    print("...IIIIIIIIIIII...**...AA........AA...**...AA........AA....**")
+    
+    print()
+
+
+main()
